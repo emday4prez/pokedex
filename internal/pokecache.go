4872b7f3310ffa824dd8a936ac61ada4ca806ffb
Emerson <emerson.day@gmail.com>
2024-04-17 18:44:53 -0700

    get from cache map

--- internal/pokecache.go
+++ internal/pokecache.go
@@ -15,5 +15,32 @@ type Cache struct {
 	cacheMap  map[string]cacheEntry
 }
 
+func (c *Cache) Add(key string, val []byte){
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+		c.cacheMap[key] = cacheEntry{
+			val: val,
+			createdAt: time.Now(),
+	}
+}
+
+func (c *Cache) Get(key string) ([]byte, bool){
+ c.mu.Lock()
+	defer c.mu.Unlock()
+	
+	entry, exists := c.cacheMap[key]
+	if exists {
+		return entry.val, true
+	}else{
+		return nil, false;
+	}
+
+}
+
+func NewCache(interval time.Duration)Cache {
+	
+	return Cache{}
+}
 
 
